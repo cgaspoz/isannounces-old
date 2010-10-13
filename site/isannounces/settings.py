9cf38c93398acda88a5972631bacf0d5dc85c01d
Cedric Gaspoz <cedric@gaspoz-fleiner.com>
2010-10-13 17:50:50 +0000

    Added a nice template inspired by github, should rewrite it in the next future

--- site/isannounces/settings.py
+++ site/isannounces/settings.py
@@ -12,11 +12,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'isannounces.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'isannounces',                      # Or path to database file if using sqlite3.
+        'USER': 'isandev',                      # Not used with sqlite3.
+        'PASSWORD': 'fNnAGVcF32SPPMU6',                  # Not used with sqlite3.
+        'HOST': 'isannounces.org',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
@@ -82,6 +82,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    'C:/Documents and Settings/cgaspoz/django-dev/isannounces/templates',
 )
 
 INSTALLED_APPS = (
